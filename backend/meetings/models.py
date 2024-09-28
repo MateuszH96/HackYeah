43f374af297bdb6da977ff86f46bdcdb2ae7ed6a
jacekku <kecajwy@gmail.com>
2024-09-28 13:34:32 +0200

    models

---
+++ backend/meetings/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Location(models.Model):
+    google_url = models.URLField(null=True, blank=True)  # Allow null in DB and blank in forms
+    lat_long = models.CharField(max_length=255, null=True, blank=True)  # Allow null values
+
+    def __str__(self):
+        return self.google_url or "Unknown Location"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False)  # Non-nullable by default
+
+    def __str__(self):
+        return self.name
+
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=200, null=False)  # Non-nullable
+    description = models.TextField(null=True, blank=True)  # Nullable and optional in forms
+    date = models.DateTimeField(null=True, blank=True)  # Nullable if the meeting date isn't set
+    categories = models.ManyToManyField(Category)  # Many-to-Many fields are nullable by default
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)  # Allow nullable foreign key
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set when created, non-nullable
+    deleted = models.BooleanField(default=False)  # Non-nullable but with a default value
+
+    def __str__(self):
+        return self.title
