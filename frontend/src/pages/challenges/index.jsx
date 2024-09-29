b35be4e9a5fc803721f400d3ec0fb77e48198711
jacekku <kecajwy@gmail.com>
2024-09-29 02:13:40 +0200

    haronon

--- frontend/src/pages/challenges/index.jsx
+++ frontend/src/pages/challenges/index.jsx
@@ -103,8 +103,8 @@ function Challenge() {
                 </div>
             )}
 
-            <MapContainer style={{ width: "500px", height: "500px" }} center={userLocation ? [userLocation.latitude, userLocation.longitude] : [51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
+            <MapContainer className="map" zoom={13} scrollWheelZoom={false}>
+                <TileLayer className="map"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
